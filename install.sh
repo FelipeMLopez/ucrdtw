f1980a5f1a32abf9733cfb959a28779c73cdb95f
FelipeMLopez <12429461+FelipeMLopez@users.noreply.github.com>
2023-04-26 13:19:06 +0200

    Install the library in the Intel venv of Alder

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ HOSTNAME=$(hostname)
 if [ "$HOSTNAME" == "alder" ]; then
     # Install library for server1
     pip install --upgrade --target=$HOME/venv/lib/python3.8/site-packages .
-    # pip install --upgrade --target=/opt/intel/oneapi/intelpython/python3.9/envs/2022.2.1/lib/python3.9/site-packages .
+    pip install --upgrade --target=$HOME/.conda/envs/fmlopez_intelpython/lib/python3.9/site-packages/ .
 elif [ "$HOSTNAME" == "picasso" ]; then
     # Install library for server2
     pip install --upgrade --target=$HOME/venv/lib/python3.8/site-packages .
