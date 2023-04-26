d1ace6f340d299be038e459656f447ebc4a7cf53
FelipeMLopez <12429461+FelipeMLopez@users.noreply.github.com>
2023-04-26 12:57:49 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the hostname of the server
+HOSTNAME=$(hostname)
+
+# Check the hostname and install the appropriate library
+if [ "$HOSTNAME" == "alder" ]; then
+    # Install library for server1
+    pip install --upgrade --target=$HOME/venv/lib/python3.8/site-packages .
+    # pip install --upgrade --target=/opt/intel/oneapi/intelpython/python3.9/envs/2022.2.1/lib/python3.9/site-packages .
+elif [ "$HOSTNAME" == "picasso" ]; then
+    # Install library for server2
+    pip install --upgrade --target=$HOME/venv/lib/python3.8/site-packages .
+else
+    echo "Unknown server"
+fi
+
+# python setup.py build && python setup.py install --install-lib ../venv/lib/python3.9/site-packages/
+# pip install --upgrade --target=$HOME/venv/lib/python3.8/site-packages .
+# pip install --upgrade --target=/opt/intel/oneapi/intelpython/python3.9/envs/2022.2.1/lib/python3.9/site-packages .
